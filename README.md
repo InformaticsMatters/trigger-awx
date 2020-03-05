5c46d8f26ce2d2bb750125990e02ca48204aeef3
Alan Christie <alan.christie@matildapeak.com>
2020-03-05 12:12:30 +0000

    - Initial code

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-trigger-awx
+# trigger-awx
+
+A utility to trigger (execute) Job Templates on a designated AWX server
+using the [Ansible Tower CLI].
+
+# Use in `.travis.yml`
+To make a Travis build trigger a Job Template n an AWX server, do two things:
+
+1.  Set appropriate environment variables as described in the
+    embedded documentation in the `trigger-awx.sh` script.
+    These can be defined in the Travis console for the project (refer to the
+    [Environment Variables] documentation on Travis).
+   
+    You will need the AWX server URL, a job name and credentials for a user
+    that can execute the chosen job.
+
+2.  Add a `trigger awx` stage to your `.travis.yml` and then add the following:
+
+```
+language: python
+python:
+- '3.8'
+
+env:
+  global:
+  # The origin of the trigger code
+  - TRIGGER_ORIGIN=https://raw.githubusercontent.com/informaticsmatters/trigger-awx/master
+
+install:
+- curl --location --retry 3 ${TRIGGER_ORIGIN}/requirements.txt --output trigger-awx-requirements.txt
+- curl --location --retry 3 ${TRIGGER_ORIGIN}/trigger-travis.py --output trigger-awx.py
+- pip install -r trigger-awx-requirements.txt
+- chmod +x trigger-awx.sh
+
+jobs:
+  include:
+  - stage: trigger awx
+    script: ./trigger-awx.sh
+```
+
+---
+
+[ansible tower cli]: https://pypi.org/project/ansible-tower-cli/ 
+[environment variables]: https://docs.travis-ci.com/user/environment-variables/
---
+++ trigger-awx.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# A script to trigger an AWX Job Template.
+#
+# This assumes the 'tower-cli' utility is available,
+# usually installed via a requirements file prior to our execution.
+#
+# usage: ./trigger-awx.sh
+#
+# As this scritp is normally executed from wthin a Travis VM
+# it uses environment vraiables to control the script's actions. Namely: -
+#
+# - AWX_JOB_NAME      The name of the Job Template to execute
+# - AWX_HOST          The server URL (i.e. https://example.com/awx)
+# - AWX_USER          The name of a user that can execute the Job
+# - AWX_USER_PASSWORD The user's password
+#
+# The script disabes any input that the AWX Job Template may request and
+# then waits for the Job to complete.
+#
+# If the Job Template is not found the script exits with code 1.
+
+set -eo pipefail
+
+: "${AWX_JOB_NAME?Need to set AWX_JOB_NAME}"
+: "${AWX_HOST?Need to set AWX_HOST}"
+: "${AWX_USER?Need to set AWX_USER}"
+: "${AWX_USER_PASSWORD?Need to set AWX_USER_PASSWORD}"
+
+echo "Getting Job Template ID for '$AWX_JOB_NAME' from ${AWX_HOST}..."
+# Get the AWX Job Template ID from the expected Job Template name
+jtid=$(tower-cli job_template list -n "$AWX_JOB_NAME" -f id \
+  -h "$AWX_HOST" \
+  -u "$AWX_USER" \
+  -p "$AWX_USER_PASSWORD")
+
+# If we have a template ID (i.e. a number) then trigger it
+# and disable any input that might be expected by the Job.
+if [[ $jtid =~ ^[0-9]+$ ]]; then
+  echo "Launching Job ID ${jtid} and waiting..."
+  tower-cli job launch -J "$jtid" --no-input --wait \
+    -h "$AWX_HOST" \
+    -u "$AWX_USER" \
+    -p "$AWX_USER_PASSWORD"
+else
+  echo "Job Template '$AWX_JOB_NAME' does not exist ($jtid)"
+  exit 1
+fi
